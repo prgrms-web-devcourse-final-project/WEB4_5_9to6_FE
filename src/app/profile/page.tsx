a90195401977b13222bd774288fdc01fd727b205
79gun79 <79gun79@gmail.com>
2025-07-08 10:17:58 +0900

    feat: ProfileCard 생성

--- src/app/profile/page.tsx
+++ src/app/profile/page.tsx
@@ -1,8 +1,10 @@
+import ProfileCard from "@/components/ProfileCard";
+
 export default function page() {
     return (
         <>
             <div className="w-90">
-                <h1 className="text-orange400 h1">글꼴 색상 확인</h1>
+                <ProfileCard />
             </div>
         </>
     );
---
+++ src/components/ProfileCard.tsx
@@ -0,0 +1,17 @@
+"use client";
+
+export default function ProfileCard() {
+    return (
+        <>
+            <div className="flex">
+                <div className="flex flex-col">
+                    <p className="text-2xl font-bold">죽음의고양이</p>
+                    <p>가입된 스터디 3개</p>
+                    <div className="flex">
+                        <h2></h2>
+                    </div>
+                </div>
+            </div>
+        </>
+    );
+}
