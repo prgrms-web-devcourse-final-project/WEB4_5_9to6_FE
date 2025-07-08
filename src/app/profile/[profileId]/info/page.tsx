84ae69660bbd6ea9e217275abebd97a46e975f10
79gun79 <79gun79@gmail.com>
2025-07-08 20:38:00 +0900

    feat: 마이페이지 수정부분 작성중

--- src/app/profile/[profileId]/info/page.tsx
+++ src/app/profile/[profileId]/info/page.tsx
@@ -6,7 +6,7 @@ export default function page() {
     return (
         <>
             <div className="flex justify-center">
-                <div className="bg-gray100 min-h-screen w-90 pb-4">
+                <div className="min-h-screen w-90 bg-white pb-4">
                     <div className="flex flex-col items-center justify-center gap-5 pt-4 pb-12">
                         <span className="not-only:bg-gray200 relative flex h-26 w-26 items-center justify-center rounded-[40px]">
                             <Image
@@ -22,14 +22,14 @@ export default function page() {
                             죽음의고양이
                         </p>
                     </div>
-                    <div className="border-t-gray300 border-b-gray300 mx-5 flex flex-col border-t border-b">
+                    <div className="border-t-gray300 border-b-gray300 mx-5 flex flex-col border-t border-b py-5">
                         <p className="b2 text-gray1000">내 계정</p>
                     </div>
-                    <div className="border-b-gray300 mx-5 flex flex-col border-b">
-                        <p className="b2 text-gray1000">내 계정</p>
+                    <div className="border-b-gray300 mx-5 flex flex-col border-b py-5">
+                        <p className="b2 text-gray1000">닉네임</p>
                     </div>
-                    <div className="border-b-gray300 mx-5 flex flex-col border-b">
-                        <p className="b2 text-gray1000">내 계정</p>
+                    <div className="border-b-gray300 mx-5 flex flex-col border-b py-5">
+                        <p className="b2 text-gray1000">비밀번호 변경</p>
                     </div>
                 </div>
             </div>
