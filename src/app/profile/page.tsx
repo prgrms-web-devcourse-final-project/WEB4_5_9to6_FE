fc035202e4c9c069a241dc3e8d6a5edd09352153
79gun79 <79gun79@gmail.com>
2025-07-08 16:40:13 +0900

    feat: 스터디 선택 모달 추가

--- src/app/profile/page.tsx
+++ src/app/profile/page.tsx
@@ -1,12 +1,16 @@
+import MyStudyModal from "@/components/profile/MyStudyModal";
 import ProfileCard from "@/components/profile/ProfileCard";
 import ProfileTabs from "@/components/profile/ProfileTabs";
 
 export default function page() {
     return (
         <>
-            <div className="bg-gray100 min-h-screen w-90 pb-4">
-                <ProfileCard />
-                <ProfileTabs />
+            <div className="flex justify-center">
+                <div className="bg-gray100 min-h-screen w-90 pb-4">
+                    <ProfileCard />
+                    <ProfileTabs />
+                    <MyStudyModal />
+                </div>
             </div>
         </>
     );
--- src/components/profile/MyLogList.tsx
+++ src/components/profile/MyLogList.tsx
@@ -4,8 +4,11 @@ import Image from "next/image";
 import trophy from "../../assets/images/trophy.png";
 import { ChevronDown } from "lucide-react";
 import LogPerWeek from "./LogPerWeek";
+import { useMyStudyModalStore } from "@/stores/myStudyModalStore";
 
 export default function MyLogList() {
+    const { openModal } = useMyStudyModalStore();
+
     return (
         <>
             <div className="flex w-full flex-col items-center gap-6 py-8">
@@ -16,12 +19,15 @@ export default function MyLogList() {
             <div className="bg-gray200 h-4 w-full"></div>
             <div className="mx-5 my-6 flex w-full flex-col gap-4">
                 <h3 className="text-gray1000">스터디별 활동 현황</h3>
-                <div className="bg-gray200 hover:bg-gray300 flex w-80 cursor-pointer justify-between rounded-2xl p-5">
+                <button
+                    onClick={openModal}
+                    className="bg-gray200 hover:bg-gray300 flex w-80 cursor-pointer justify-between rounded-2xl p-5"
+                >
                     <h5 className="text-gray1000">
                         수코딩 강의로 배우는 프론트엔드
                     </h5>
                     <ChevronDown size={18} className="text-gray500" />
-                </div>
+                </button>
                 <LogPerWeek />
             </div>
         </>
