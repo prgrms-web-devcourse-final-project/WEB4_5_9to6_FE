c0a86b3165fe010f624bc6cda2f85abb9d1af3c7
Bomin Kim <sirubom1203@gmail.com>
2025-07-09 00:00:15 +0900

    active style설정

--- src/components/common/Gnb.tsx
+++ src/components/common/Gnb.tsx
@@ -1,32 +1,58 @@
+"use client";
 import { House, Store, ClipboardList, UserRound } from "lucide-react";
+import Link from "next/link";
+import { usePathname } from "next/navigation";
 
 export default function Gnb() {
+    const pathname = usePathname();
     return (
         <>
             <div className="bottom-0 h-18 w-full bg-[var(--color-gray300)]">
                 <div className="mx-10 flex justify-between pt-4">
-                    <div className="flex cursor-pointer flex-col items-center">
-                        <House className="mb-1 justify-center text-[var(--color-gray1000)]" />
-                        <p className="c2 text-[var(--color-gray1000)]">홈</p>
-                    </div>
-                    <div className="flex cursor-pointer flex-col items-center">
-                        <ClipboardList className="mb-1 justify-center text-[var(--color-gray1000)]" />
-                        <p className="c2 text-[var(--color-gray1000)]">
-                            스터디 목록
-                        </p>
-                    </div>
-                    <div className="flex cursor-pointer flex-col items-center">
-                        <Store className="mb-1 justify-center text-[var(--color-gray1000)]" />
-                        <p className="c2 text-[var(--color-gray1000)]">
-                            리워드 상점
-                        </p>
-                    </div>
-                    <div className="flex cursor-pointer flex-col items-center">
-                        <UserRound className="mb-1 justify-center text-[var(--color-gray1000)]" />
-                        <p className="c2 text-[var(--color-gray1000)]">
-                            마이페이지
-                        </p>
-                    </div>
+                    <Link
+                        href="/"
+                        className={`flex cursor-pointer flex-col items-center ${
+                            pathname === "/"
+                                ? "text-[var(--color-gray1000)]"
+                                : "text-[var(--color-gray500)]"
+                        }`}
+                    >
+                        <House className={`mb-1 justify-center`} />
+                        <p className="c2">홈</p>
+                    </Link>
+                    <Link
+                        href="/studylist"
+                        className={`flex cursor-pointer flex-col items-center ${
+                            pathname === "/studylist"
+                                ? "text-[var(--color-gray1000)]"
+                                : "text-[var(--color-gray500)]"
+                        }`}
+                    >
+                        <ClipboardList className="mb-1 justify-center" />
+                        <p className="c2">스터디 목록</p>
+                    </Link>
+                    <Link
+                        href="/rewardshop"
+                        className={`flex cursor-pointer flex-col items-center ${
+                            pathname === "/rewardshop"
+                                ? "text-[var(--color-gray1000)]"
+                                : "text-[var(--color-gray500)]"
+                        }`}
+                    >
+                        <Store className="] mb-1 justify-center" />
+                        <p className="c2">리워드 상점</p>
+                    </Link>
+                    <Link
+                        href="/profile"
+                        className={`flex cursor-pointer flex-col items-center ${
+                            pathname === "/profile"
+                                ? "text-[var(--color-gray1000)]"
+                                : "text-[var(--color-gray500)]"
+                        }`}
+                    >
+                        <UserRound className="mb-1 justify-center" />
+                        <p className="c2">마이페이지</p>
+                    </Link>
                 </div>
             </div>
         </>
