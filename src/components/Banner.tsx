7af834491683ee594ebb1251233a7fce1b34ecf9
Bomin Kim <sirubom1203@gmail.com>
2025-07-08 23:02:54 +0900

    style: banner slide

--- src/components/Banner.tsx
+++ src/components/Banner.tsx
@@ -1,51 +1,60 @@
+"use client";
+
 import Image from "next/image";
 import { Swiper, SwiperSlide } from "swiper/react";
 import "swiper/css";
+import "swiper/css/navigation";
 import "swiper/css/pagination";
-import { Pagination } from "swiper/modules";
+import "swiper/css/scrollbar";
+import { Autoplay, Pagination, Navigation } from "swiper/modules";
+import { ChevronLeft, ChevronRight } from "lucide-react";
 
 export default function Banner() {
+    const bannerImages = [
+        "/images/banner/banner02.png",
+        "/images/banner/banner01.png",
+        "/images/banner/banner03.png",
+        "/images/banner/banner04.png",
+        "/images/banner/banner05.png",
+    ];
     return (
         <>
-            <Swiper
-                pagination={true}
-                modules={[Pagination]}
-                className="mySwiper"
-            >
-                <SwiperSlide>
-                    <Image
-                        src="/images/banner01.png"
-                        alt="banner1"
-                        width={320}
-                        height={90}
-                        sizes="640"
-                    />
-                </SwiperSlide>
-                <SwiperSlide>
-                    <Image
-                        src="/images/banner05.png"
-                        alt="banner5"
-                        width={320}
-                        height={90}
-                    />
-                </SwiperSlide>
-                <SwiperSlide>
-                    <Image
-                        src="/images/banner03.png"
-                        alt="banner3"
-                        width={320}
-                        height={90}
-                    />
-                </SwiperSlide>
-                <SwiperSlide>
-                    <Image
-                        src="/images/banner04.png"
-                        alt="banner4"
-                        width={320}
-                        height={90}
-                    />
-                </SwiperSlide>
-            </Swiper>
+            <div className="mt-3 w-full">
+                <Swiper
+                    spaceBetween={50}
+                    slidesPerView={1}
+                    autoplay={{ delay: 5000 }}
+                    navigation={{
+                        nextEl: ".swiper-button-next-custom",
+                        prevEl: ".swiper-button-prev-custom",
+                    }}
+                    pagination={{
+                        clickable: true,
+                        type: "bullets",
+                    }}
+                    modules={[Pagination, Autoplay, Navigation]}
+                    className="w-5/7"
+                >
+                    <div className="swiper-button-prev-custom">
+                        <ChevronLeft />
+                    </div>
+                    {bannerImages.map((src, idx) => (
+                        <SwiperSlide key={idx}>
+                            <Image
+                                src={src}
+                                alt={`banner${idx + 1}`}
+                                width={0}
+                                height={0}
+                                sizes="100vw"
+                                className="w-full rounded-2xl"
+                            />
+                        </SwiperSlide>
+                    ))}
+                    <div className="swiper-button-next-custom">
+                        <ChevronRight />
+                    </div>
+                </Swiper>
+            </div>
         </>
     );
 }
