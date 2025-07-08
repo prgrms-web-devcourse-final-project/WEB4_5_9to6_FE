8219e492bfe77e87f51ef742a0c9e3e663cbd7d5
Bomin Kim <sirubom1203@gmail.com>
2025-07-08 23:32:54 +0900

    header 수정

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,7 @@
-import Banner from "@/components/Banner";
-
 export default async function Main() {
     return (
         <>
             <div className="pt-16">
-                <Banner />
                 <h1>page</h1>
             </div>
         </>
