446c77ae98e5a8e47e4e7c22097715d5b4bbcffc
Hayoung Kang <rkdgkdudsam@naver.com>
2025-07-09 09:46:25 +0900

    design: Button disabled 스타일 추가

--- src/components/common/Button.tsx
+++ src/components/common/Button.tsx
@@ -9,20 +9,23 @@ export default function Button({
     color = "black",
     icon,
     children,
+    disabled,
     ...props
 }: ButtonProps) {
     return (
         <>
             <button
-                className={`h5 h-[50px] w-full cursor-pointer rounded-[12px] transition-all duration-200 ease-in-out ${
-                    color === "black"
-                        ? "bg-[var(--color-gray1000)] text-white hover:bg-[var(--color-gray900)]"
-                        : color === "primary"
-                          ? "bg-[var(--color-main500)] text-white hover:bg-[var(--color-main600)]"
-                          : color === "yellow"
-                            ? "bg-[#F9E95A] text-[#191919] hover:bg-[#EFDE3E]"
-                            : "border border-[var(--color-gray300)] text-[#191919] hover:bg-[var(--color-gray100)]"
-                }`}
+                className={`h5 h-[50px] w-full rounded-[12px] transition-all duration-200 ease-in-out ${
+                    disabled
+                        ? "cursor-default bg-[var(--color-gray200)] text-[var(--color-gray500)]"
+                        : color === "black"
+                          ? "cursor-pointer bg-[var(--color-gray1000)] text-white hover:bg-[var(--color-gray900)]"
+                          : color === "primary"
+                            ? "cursor-pointer bg-[var(--color-main500)] text-white hover:bg-[var(--color-main600)]"
+                            : color === "yellow"
+                              ? "cursor-pointer bg-[#F9E95A] text-[#191919] hover:bg-[#EFDE3E]"
+                              : "cursor-pointer border border-[var(--color-gray300)] text-[#191919] hover:bg-[var(--color-gray100)]"
+                } `}
                 {...props}
             >
                 <div className="flex items-center justify-center gap-2">
