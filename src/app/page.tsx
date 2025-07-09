92d9413288f25ee5cacffe003e47ca5388a7652c
Bomin Kim <sirubom1203@gmail.com>
2025-07-09 16:18:30 +0900

    header, gnb 메인페이지로 이동

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,17 @@
 import Banner from "@/components/Banner";
+import Gnb from "@/components/common/Gnb";
+import Header from "@/components/common/Header";
 import StudyTime from "@/components/StudyTime";
 
 export default async function Main() {
     return (
         <>
+            <Header />
             <div className="min-h-screen bg-[var(--color-gray100)] px-5 pt-16">
                 <Banner />
                 <StudyTime />
             </div>
+            <Gnb />
         </>
     );
 }
