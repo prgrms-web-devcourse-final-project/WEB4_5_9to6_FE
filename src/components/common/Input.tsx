8859c5d001491ca5c35b33266c38e3a62addd516
Hayoung Kang <rkdgkdudsam@naver.com>
2025-07-09 12:19:58 +0900

    feat: 공용 Input 컴포넌트

---
+++ src/components/common/Input.tsx
@@ -0,0 +1,26 @@
+import React from "react";
+
+interface InputProps extends React.InputHTMLAttributes<HTMLInputElement> {
+    error?: boolean;
+    errorMsg?: string;
+}
+
+export default function Input({ error, errorMsg, ...props }: InputProps) {
+    return (
+        <>
+            <input
+                className={`h-[54px] w-full rounded-[12px] border border-[var(--color-gray-300)] pl-4 text-[var(--color-gray1000)] placeholder-[var(--color-gray500)] duration-200 ease-in-out focus:outline-none ${error && "ring-1 ring-[#FF394A]"}`}
+                {...props}
+            />
+            <label
+                className={`h6 overflow-hidden pl-2 text-[#FF394A] transition-all duration-200 ease-in-out ${
+                    error
+                        ? "max-h-[40px] translate-y-0 opacity-100"
+                        : "max-h-0 -translate-y-5 opacity-0"
+                }`}
+            >
+                {errorMsg}
+            </label>
+        </>
+    );
+}
