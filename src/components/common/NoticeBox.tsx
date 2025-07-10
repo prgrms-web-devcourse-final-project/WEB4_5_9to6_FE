9cf4959396108a122c5d75f961ce442737429581
Bomin Kim <sirubom1203@gmail.com>
2025-07-10 23:48:14 +0900

    style: 공지사항 열림 트랜지션 추가

--- src/components/common/NoticeBox.tsx
+++ src/components/common/NoticeBox.tsx
@@ -22,30 +22,39 @@ export default function NoticeBox({
 
     return (
         <>
-            <div className="fixed h-fit w-full rounded-2xl bg-white/85 px-4 py-3 backdrop-blur">
-                <p className="c2 text-[var(--color-gray700)]">
-                    {date} 공지사항
-                </p>
-                {/* 80글자 제한 */}
-                <div className="easy-in-out flex overflow-hidden transition-all duration-300">
-                    <p
-                        className={`c1 mr-4 leading-4.5 text-[var(--color-gray-1000)] ${expanded ? "line-clamp-none" : "line-clamp-1 overflow-hidden text-ellipsis whitespace-nowrap"}`}
-                    >
-                        {content}
+            <div
+                className={`relative h-fit rounded-2xl bg-white/85 px-4 py-3.5 backdrop-blur ${className}`}
+            >
+                <div className="flex items-center justify-between">
+                    <p className="c2 text-[var(--color-gray700)]">
+                        {date} 공지사항
                     </p>
                     <button
                         onClick={toggleHandler}
-                        className="cursor-pointer pb-2"
+                        className="mt-3.5 cursor-pointer"
                     >
                         {expanded ? <ChevronUp /> : <ChevronDown />}
                     </button>
+                </div>
+                {/* 80글자 제한 */}
+                <div
+                    style={{
+                        maxHeight: expanded ? "500px" : "30px",
+                    }}
+                    className="relative flex overflow-hidden transition-all duration-700"
+                >
+                    <p
+                        className={`c1 mr-5 leading-4.5 text-[var(--color-gray-1000)] ${expanded ? "line-clamp-none" : "line-clamp-1 overflow-hidden text-ellipsis whitespace-nowrap"}`}
+                    >
+                        {content}
+                    </p>
                 </div>
                 <div
                     className={`mt-3 ${expanded ? "flex" : "hidden"} flex-col items-center`}
                 >
                     <button
                         onClick={toggleHandler}
-                        className="h6 cursor-pointer text-[var(--color-gray1000)]"
+                        className="h6 right-0 cursor-pointer text-[var(--color-gray1000)]"
                     >
                         접기
                     </button>
