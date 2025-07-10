d68683047500b27c272d4f0701e2dba448b85a87
79gun79 <79gun79@gmail.com>
2025-07-10 09:56:48 +0900

    zap: 공용컴포넌트 사용

--- src/components/profile/info/ChangeNickname.tsx
+++ src/components/profile/info/ChangeNickname.tsx
@@ -1,6 +1,7 @@
 "use client";
 
 import Button from "@/components/common/Button";
+import Input from "@/components/common/Input";
 
 export default function ChangeNickname() {
     return (
@@ -8,11 +9,7 @@ export default function ChangeNickname() {
             <div className="flex h-full flex-col justify-between bg-white p-5">
                 <div className="flex flex-col">
                     <p className="b2 text-gray1000 mb-2">닉네임</p>
-                    <input
-                        type="text"
-                        className="border-gray300 b1 rounded-xl border p-4"
-                        placeholder="닉네임을 입력하세요"
-                    />
+                    <Input type="text" placeholder="닉네임을 입력하세요" />
                 </div>
 
                 <Button color="black">변경하기</Button>
