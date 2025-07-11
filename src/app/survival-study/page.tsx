11fdeb1f97097f779c9b5fefd6089ce5dfc897c9
Bomin Kim <sirubom1203@gmail.com>
2025-07-11 12:23:27 +0900

    style: 서바이벌스터디 메인페이지

--- src/app/survival-study/page.tsx
+++ src/app/survival-study/page.tsx
@@ -1,25 +1,56 @@
-import BackButton from "@/components/common/BackButton";
-import NoticeBox from "@/components/common/NoticeBox";
-import Image from "next/image";
+"use client";
 
-export default async function SurvivalStudy() {
+import BackButton from "@/components/common/BackButton";
+import Button from "@/components/common/Button";
+import NoticeBox from "@/components/common/NoticeBox";
+import SurvivalInfo from "@/components/survival/SurvivalInfo";
+import Image from "next/image";
+import Link from "next/link";
+
+export default function SurvivalStudy() {
     return (
         <>
-            <div className="relative">
-                <Image
-                    src="/images/roomImgs/room7.png"
-                    alt="survival study"
-                    width={1000}
-                    height={470}
-                    style={{
-                        width: "100%",
-                        maxHeight: "500px",
-                    }}
-                />
-                <div className="absolute top-4 left-4">
-                    <BackButton />
+            <div className="h-screen">
+                <div className="relative">
+                    <Image
+                        src="/images/roomImgs/room7.png"
+                        alt="survival study"
+                        width={1000}
+                        height={470}
+                        style={{
+                            width: "100%",
+                            maxHeight: "500px",
+                        }}
+                    />
+                    <div className="absolute top-4 left-4">
+                        <BackButton />
+                    </div>
+                    <button className="absolute right-4 bottom-4 h-6.5 w-14.5 rounded-3xl bg-[#1d1d1d]/80">
+                        <Link href="/profile/123/theme">
+                            <p className="c2 px-2 pt-0.5 text-white">
+                                테마변경
+                            </p>
+                        </Link>
+                    </button>
                 </div>
-                <NoticeBox />
+
+                <NoticeBox
+                    content="다음 퀴즈는 7/17 열시에 열릴 예정입니다."
+                    className="rounded-none bg-[var(--color-gray100)]/85"
+                    date="07.11"
+                />
+                <SurvivalInfo />
+                <div className="absolute bottom-0 flex h-22.5 w-full justify-center">
+                    <Button className="mx-5 my-5 bg-[var(--color-main500)] transition duration-200 hover:bg-[var(--color-main600)]">
+                        <Image
+                            src="/icons/flash.svg"
+                            alt="survival icon"
+                            width={14}
+                            height={14}
+                        />
+                        서바이벌
+                    </Button>
+                </div>
             </div>
         </>
     );
---
+++ src/components/survival/SurvivalInfo.tsx
@@ -0,0 +1,61 @@
+import Image from "next/image";
+
+export default function SurvivalInfo() {
+    const schedule = [
+        { goal: "토익 문법, 어휘 2/5 이상 점수 시 생존", reward: "200" },
+        { goal: "토익 문법, 어휘 2/5 이상 점수 시 생존", reward: "300" },
+        { goal: "토익 문법, 어휘 2/5 이상 점수 시 생존", reward: "400" },
+        { goal: "토익 문법, 어휘 2/5 이상 점수 시 생존", reward: "500" },
+    ];
+    return (
+        <>
+            <div className="mt-6 w-full px-5">
+                <div className="flex h-6 items-center justify-center gap-1.5">
+                    <div className="c2 h-full rounded-lg bg-[var(--color-gray1000)] px-2 text-white">
+                        프로그래밍
+                    </div>
+                    <div className="c2 flex h-full rounded-lg bg-[var(--color-main400)] px-2 text-white">
+                        <Image
+                            src="/icons/flash.svg"
+                            alt="survival icon"
+                            width={14}
+                            height={14}
+                        />
+                        서바이벌
+                    </div>
+                </div>
+                <h2 className="h2 mt-[15px] flex justify-center text-[var(--color-gray1000)]">
+                    토익 고급 살아남기
+                </h2>
+                <p className="mt-1.5 text-center text-[var(--color-gray700)]">
+                    매주 수요일 · 15:00~18:00 · 온라인
+                </p>
+                <h4 className="h4 mt-6.5 text-center">참가자 : 30명</h4>
+            </div>
+            <hr className="mx-5 mt-6 text-[var(--color-gray200)]" />
+            <div className="w-full px-5">
+                <h3 className="mt-6">서바이벌 목표</h3>
+                <div className="mt-[10px] flex flex-col gap-2">
+                    {schedule.map((schedule, index) => (
+                        <div
+                            key={index}
+                            className="flex h-[50px] w-full items-center justify-between rounded-[12px] bg-[var(--color-gray100)] px-4 py-4"
+                        >
+                            <div className="flex">
+                                <p className="c1 mr-3 font-medium text-[var(--color-gray1000)]">
+                                    {index + 1}주차
+                                </p>
+                                <p className="c1 text-[var(--color-gray1000)]">
+                                    {schedule.goal}
+                                </p>
+                            </div>
+                            <p className="text-[var(--color-main400)]">
+                                {schedule.reward}P
+                            </p>
+                        </div>
+                    ))}
+                </div>
+            </div>
+        </>
+    );
+}
