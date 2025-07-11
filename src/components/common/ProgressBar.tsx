2e70820be33426f87cec33550b42f0d6c9785364
Hayoung Kang <rkdgkdudsam@naver.com>
2025-07-11 15:31:33 +0900

    zap: ProgressBar totalStep 추가

--- src/components/common/ProgressBar.tsx
+++ src/components/common/ProgressBar.tsx
@@ -1,14 +1,20 @@
 import { useEffect, useState } from "react";
 
-export default function ProgressBar({ step }: { step: number }) {
+export default function ProgressBar({
+    totalStep,
+    step,
+}: {
+    totalStep: number;
+    step: number;
+}) {
     const [progressWidth, setProgressWidth] = useState(0);
 
     useEffect(() => {
-        setProgressWidth(step * 20);
-    }, [step]);
+        setProgressWidth(step * (100 / totalStep));
+    }, [step, totalStep]);
 
     return (
-        <div className="fixed top-[62px] h-[3px] w-full bg-[var(--color-gray100)]">
+        <div className="fixed top-[62px] h-[3px] w-full max-w-sm bg-[var(--color-gray100)]">
             <div
                 className={`absolute top-0 left-0 h-full bg-[var(--color-main400)] duration-1500 ease-out`}
                 style={{
@@ -16,10 +22,15 @@ export default function ProgressBar({ step }: { step: number }) {
                 }}
             />
 
-            <div className="absolute top-0 left-[calc(20%-2px)] h-full w-[4px] bg-white" />
-            <div className="absolute top-0 left-[calc(40%-2px)] h-full w-[4px] bg-white" />
-            <div className="absolute top-0 left-[calc(60%-2px)] h-full w-[4px] bg-white" />
-            <div className="absolute top-0 left-[calc(80%-2px)] h-full w-[4px] bg-white" />
+            {[...Array(totalStep - 1)].map((_, i) => (
+                <div
+                    key={i}
+                    className="absolute top-0 h-full w-[4px] bg-white"
+                    style={{
+                        left: `calc(${((i + 1) * 100) / totalStep}% - 2px)`,
+                    }}
+                />
+            ))}
         </div>
     );
 }
