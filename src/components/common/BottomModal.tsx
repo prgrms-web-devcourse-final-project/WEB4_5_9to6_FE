b23c70a6085bf38ca6262c5870abdd78f662cc55
79gun79 <79gun79@gmail.com>
2025-07-16 14:56:05 +0900

    feat: 로그아웃 기능 추가

--- src/components/common/BottomModal.tsx
+++ src/components/common/BottomModal.tsx
@@ -48,24 +48,23 @@ export default function BottomModal({
             <div
                 className={`fixed inset-0 z-50 bg-[#000000]/30 duration-200 ${animationClass === "animate-modalFadeIn" ? "opacity-100" : "opacity-0"}`}
                 onClick={closeHandler}
+            ></div>
+            <div
+                className={`${
+                    animationClass
+                } fixed right-[10px] bottom-5 left-[10px] z-50 flex flex-col rounded-3xl bg-[#FFFFFF] py-5`}
+                style={{ height: `${height}px` }}
             >
-                <div
-                    className={`${
-                        animationClass
-                    } absolute right-[10px] bottom-5 left-[10px] z-50 flex flex-col rounded-3xl bg-[#FFFFFF] py-5`}
-                    style={{ height: `${height}px` }}
-                >
-                    <div className="flex items-center justify-between">
-                        <h3 className="ml-5 text-[var(--color-gray1000)]">
-                            {title}
-                        </h3>
-                        <X
-                            className="mr-5 h-6 w-6 cursor-pointer text-[#161616]"
-                            onClick={closeHandler}
-                        />
-                    </div>
-                    {children}
+                <div className="flex items-center justify-between">
+                    <h3 className="ml-5 text-[var(--color-gray1000)]">
+                        {title}
+                    </h3>
+                    <X
+                        className="mr-5 h-6 w-6 cursor-pointer text-[#161616]"
+                        onClick={closeHandler}
+                    />
                 </div>
+                {children}
             </div>
         </>
     );
