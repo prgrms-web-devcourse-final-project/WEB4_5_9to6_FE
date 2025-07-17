1f7acaab6d883fa48e2086a9d27bdb5cea0b3a99
Bomin Kim <sirubom1203@gmail.com>
2025-07-17 14:29:46 +0900

    test: 사용하지 않는 api 주석처리

--- src/api/fetchUser.ts
+++ src/api/fetchUser.ts
@@ -1,15 +1,15 @@
 import { axiosInstance } from "./index";
 
-export const fetchMyData = async () => {
-    const response = await axiosInstance.get("members/info-all");
-    return response.data.data;
-};
+// export const fetchMyData = async () => {
+//     const response = await axiosInstance.get("members/info-all");
+//     return response.data.data;
+// };
 
-export const fetchStudyMember = async () => {
-    const response = await axiosInstance.get("members/23/info");
-    console.log(response.data.data);
-    return response.data.data;
-};
+// export const fetchStudyMember = async () => {
+//     const response = await axiosInstance.get("members/23/info");
+//     console.log(response.data.data);
+//     return response.data.data;
+// };
 
 export const fetchLeaderAvatar = async (id: number) => {
     const response = await axiosInstance.get(`studies/${id}/members`);
