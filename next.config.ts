367cc77b12ddba7eb899d382661aff86b0746237
Hayoung Kang <rkdgkdudsam@naver.com>
2025-07-25 15:23:26 +0900

    chore: next/image 외부 S3 이미지 도메인 허용

--- next.config.ts
+++ next.config.ts
@@ -1,5 +1,9 @@
 import type { NextConfig } from "next";
 
-const nextConfig: NextConfig = {};
+const nextConfig: NextConfig = {
+    images: {
+        domains: ["team06-9to6.s3.ap-northeast-2.amazonaws.com"],
+    },
+};
 
 export default nextConfig;
