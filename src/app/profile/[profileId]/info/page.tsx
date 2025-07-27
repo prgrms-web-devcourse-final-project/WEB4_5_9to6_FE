66766f11a39d171634650a972ad2ff2903b78d2c
79gun79 <79gun79@gmail.com>
2025-07-27 18:52:58 +0900

    feat: 로그아웃 로딩 UI 추가

--- src/app/profile/[profileId]/info/page.tsx
+++ src/app/profile/[profileId]/info/page.tsx
@@ -1,6 +1,5 @@
 import SubHeader from "@/components/common/SubHeader";
-import MyInfoCard from "@/components/profile/info/MyInfoCard";
-import MyInfoList from "@/components/profile/info/MyInfoList";
+import MyInfo from "@/components/profile/info/MyInfo";
 
 export default async function page({
     params,
@@ -13,8 +12,7 @@ export default async function page({
             <SubHeader>내 정보 수정</SubHeader>
             <div className="flex justify-center pt-15.5">
                 <div className="h-[calc(100vh-134px)] w-screen overflow-hidden bg-white pb-4">
-                    <MyInfoCard id={profileId} />
-                    <MyInfoList />
+                    <MyInfo id={profileId} />
                 </div>
             </div>
         </>
