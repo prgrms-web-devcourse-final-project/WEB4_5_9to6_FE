1c1d54b375047dfcf7fd3e9e37ec7dbc68451433
Hayoung Kang <rkdgkdudsam@naver.com>
2025-07-30 00:02:55 +0900

    feat: 알림 모두 읽음 API 연결

--- src/app/notifications/page.tsx
+++ src/app/notifications/page.tsx
@@ -1,23 +1,41 @@
 "use client";
 
+import { readAllAlarm } from "@/api/alarms";
 import AlertMessage from "@/components/AlertMessage";
 import SubHeader from "@/components/common/SubHeader";
 import { useAlarmStore } from "@/stores/alarmStore";
+import { useMutation } from "@tanstack/react-query";
 
 export default function Notifications() {
     const alarms = useAlarmStore((state) => state.alarmList);
 
+    const { mutate: readAllNotification } = useMutation({
+        mutationFn: readAllAlarm,
+        onSuccess: () => {
+            useAlarmStore.getState().clear();
+        },
+        onError: (error) => {
+            console.error(error);
+        },
+    });
+
     return (
         <>
             <SubHeader>알림</SubHeader>
-            <button className="c2 fixed right-5 cursor-pointer pt-5 text-[var(--color-gray600)]">
+            <button
+                className="c2 fixed right-5 z-50 cursor-pointer pt-5 text-[var(--color-gray600)]"
+                onClick={() => readAllNotification()}
+            >
                 모두읽기
             </button>
             <div className="pt-15">
-                {alarms.length === 0 && <p>받은 알림이 없습니다.</p>}
-                {alarms.map((alarm, i) => (
-                    <AlertMessage key={i} alarm={alarm} />
-                ))}
+                {alarms.filter((alarm) => alarm.isRead === false).length ===
+                    0 && <p>받은 알림이 없습니다.</p>}
+                {alarms
+                    .filter((alarm) => alarm.isRead === false)
+                    .map((alarm, i) => (
+                        <AlertMessage key={i} alarm={alarm} />
+                    ))}
             </div>
         </>
     );
